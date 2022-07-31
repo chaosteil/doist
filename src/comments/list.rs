ae158e049c941205d08b6d03e7cfa727fc31db48
Dominykas Djacenko <chaosteil@gmail.com>
2022-07-30 22:34:10 -0700

    Add comments to project

--- src/comments/list.rs
+++ src/comments/list.rs
@@ -1,6 +1,7 @@
 use crate::api::rest::{Comment, FullComment};
 
 pub fn list(comments: &[Comment]) {
+    println!("Comments:");
     for comment in comments {
         println!("-----");
         println!("{}", FullComment(comment));
--- src/projects/view.rs
+++ src/projects/view.rs
@@ -1,4 +1,4 @@
-use crate::api::rest::Gateway;
+use crate::{api::rest::Gateway, comments};
 use color_eyre::{eyre::eyre, Result};
 
 use super::filter::ProjectOrInteractive;
@@ -13,7 +13,7 @@ pub async fn view(params: Params, gw: &Gateway) -> Result<()> {
     let (id, list) = params.project.project(gw).await?;
     let project = match list.project(id) {
         Some(project) => project,
-        None => return Err(eyre!("no valid project selected")),
+        None => return Err(eyre!("no valid project")),
     };
     println!("Project: {}", project.item);
     let sections = list.sections(project.id);
@@ -22,6 +22,10 @@ pub async fn view(params: Params, gw: &Gateway) -> Result<()> {
         for section in sections {
             println!("{}", section)
         }
+    }
+    if project.comment_count > 0 {
+        let comments = gw.project_comments(id).await?;
+        comments::list(&comments)
     }
     Ok(())
 }

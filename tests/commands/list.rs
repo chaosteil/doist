42a72536514434ca8cd2a2fa56bbd9b7eb20dff2
Dominykas Djacenko <chaosteil@gmail.com>
2022-08-30 10:43:26 +0300

    Improve setup a bit

--- tests/commands/list.rs
+++ tests/commands/list.rs
@@ -1,26 +1,43 @@
 use super::setup::Tool;
 use assert_cmd::prelude::*;
 use color_eyre::Result;
-use wiremock::{matchers::*, Mock, MockServer, ResponseTemplate};
+use wiremock::{matchers::*, Mock, ResponseTemplate};
 
 #[tokio::test]
 async fn list() -> Result<()> {
-    let mut cmd = Tool::init()?;
+    let mut cmd = Tool::init().await?;
 
-    let mock_server = MockServer::start().await;
     Mock::given(method("GET"))
         .and(path("/rest/v1/tasks"))
         .respond_with(
             ResponseTemplate::new(200).set_body_raw(super::fixtures::TASKS, "application/json"),
         )
-        .up_to_n_times(1)
-        .mount(&mock_server)
+        .mount(&cmd.mock)
+        .await;
+    Mock::given(method("GET"))
+        .and(path("/rest/v1/labels"))
+        .respond_with(
+            ResponseTemplate::new(200).set_body_raw(super::fixtures::LABELS, "application/json"),
+        )
+        .mount(&cmd.mock)
+        .await;
+    Mock::given(method("GET"))
+        .and(path("/rest/v1/projects"))
+        .respond_with(
+            ResponseTemplate::new(200).set_body_raw(super::fixtures::PROJECTS, "application/json"),
+        )
+        .mount(&cmd.mock)
+        .await;
+    Mock::given(method("GET"))
+        .and(path("/rest/v1/sections"))
+        .respond_with(
+            ResponseTemplate::new(200).set_body_raw(super::fixtures::SECTIONS, "application/json"),
+        )
+        .mount(&cmd.mock)
         .await;
 
-    // Connection failure
-    cmd.cmd.arg("list").assert().failure();
-    cmd.cmd.assert().failure();
-    mock_server.verify().await;
+    cmd.cmd.arg("list").assert().success();
+    cmd.mock.verify().await;
 
     Ok(())
 }
--- tests/commands/setup.rs
+++ tests/commands/setup.rs
@@ -1,21 +1,46 @@
 use assert_cmd::prelude::*;
 use color_eyre::Result;
 use doist::config::Config;
-use std::process::Command;
+use std::{env, process::Command};
+use wiremock::MockServer;
 
 pub struct Tool {
     pub cmd: Command,
     pub tmp: assert_fs::TempDir,
+    pub cfg: Config,
+    pub mock: MockServer,
+
+    old_home: String,
+}
+
+impl Drop for Tool {
+    fn drop(&mut self) {
+        env::set_var("XDG_CONFIG_HOME", &self.old_home)
+    }
 }
 
 impl Tool {
-    pub fn init() -> Result<Tool> {
+    pub async fn init() -> Result<Tool> {
         let tmp = assert_fs::TempDir::new()?;
         let mut cmd = Command::cargo_bin("doist")?;
         cmd.arg("auth")
             .arg("AUTH_KEY")
             .env("XDG_CONFIG_HOME", tmp.path())
-            .assert();
-        Ok(Tool { cmd, tmp })
+            .assert()
+            .success();
+        let old_home = env::var("XDG_CONFIG_HOME").unwrap_or("".to_string());
+        env::set_var("XDG_CONFIG_HOME", tmp.path());
+        let mock = MockServer::start().await;
+        let mut cfg = Config::load()?;
+        cfg.url = url::Url::parse(&mock.uri())?;
+        cfg.save()?;
+        let cmd = Command::cargo_bin("doist")?;
+        Ok(Tool {
+            cmd,
+            tmp,
+            cfg,
+            mock,
+            old_home,
+        })
     }
 }

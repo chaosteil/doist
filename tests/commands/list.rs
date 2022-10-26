0a4f46129d229d8ada20910441c531b45a2b380d
Dominykas Djačenko <chaosteil@gmail.com>
2022-10-25 20:22:15 -0700

    Add tests for all variants of list (#75)

--- tests/commands/list.rs
+++ tests/commands/list.rs
@@ -5,48 +5,82 @@ use wiremock::{matchers::*, Mock, ResponseTemplate};
 
 #[tokio::test]
 async fn list() -> Result<()> {
-    let cmd = Tool::init().await?;
+    struct Test {
+        args: Vec<&'static str>,
+        fetch_tasks: u64,
+        fetch_labels: u64,
+        fetch_projects: u64,
+        fetch_sections: u64,
+    }
+    let tests = vec![
+        Test {
+            args: vec!["list", "--nointeractive"],
+            fetch_tasks: 1,
+            fetch_labels: 1,
+            fetch_projects: 1,
+            fetch_sections: 1,
+        },
+        Test {
+            args: vec!["--nointeractive"],
+            fetch_tasks: 1,
+            fetch_labels: 1,
+            fetch_projects: 1,
+            fetch_sections: 1,
+        },
+        Test {
+            args: vec!["l", "--nointeractive"],
+            fetch_tasks: 1,
+            fetch_labels: 1,
+            fetch_projects: 1,
+            fetch_sections: 1,
+        },
+    ];
+    for test in tests {
+        let cmd = Tool::init().await?;
 
-    Mock::given(method("GET"))
-        .and(path("/rest/v1/tasks"))
-        .respond_with(
-            ResponseTemplate::new(200).set_body_raw(super::fixtures::TASKS, "application/json"),
-        )
-        .up_to_n_times(1)
-        .mount(&cmd.mock)
-        .await;
-    Mock::given(method("GET"))
-        .and(path("/rest/v1/labels"))
-        .respond_with(
-            ResponseTemplate::new(200).set_body_raw(super::fixtures::LABELS, "application/json"),
-        )
-        .up_to_n_times(1)
-        .mount(&cmd.mock)
-        .await;
-    Mock::given(method("GET"))
-        .and(path("/rest/v1/projects"))
-        .respond_with(
-            ResponseTemplate::new(200).set_body_raw(super::fixtures::PROJECTS, "application/json"),
-        )
-        .up_to_n_times(1)
-        .mount(&cmd.mock)
-        .await;
-    Mock::given(method("GET"))
-        .and(path("/rest/v1/sections"))
-        .respond_with(
-            ResponseTemplate::new(200).set_body_raw(super::fixtures::SECTIONS, "application/json"),
-        )
-        .up_to_n_times(1)
-        .mount(&cmd.mock)
-        .await;
+        Mock::given(method("GET"))
+            .and(path("/rest/v1/tasks"))
+            .respond_with(
+                ResponseTemplate::new(200).set_body_raw(super::fixtures::TASKS, "application/json"),
+            )
+            .up_to_n_times(test.fetch_tasks)
+            .mount(&cmd.mock)
+            .await;
+        Mock::given(method("GET"))
+            .and(path("/rest/v1/labels"))
+            .respond_with(
+                ResponseTemplate::new(200)
+                    .set_body_raw(super::fixtures::LABELS, "application/json"),
+            )
+            .up_to_n_times(test.fetch_labels)
+            .mount(&cmd.mock)
+            .await;
+        Mock::given(method("GET"))
+            .and(path("/rest/v1/projects"))
+            .respond_with(
+                ResponseTemplate::new(200)
+                    .set_body_raw(super::fixtures::PROJECTS, "application/json"),
+            )
+            .up_to_n_times(test.fetch_projects)
+            .mount(&cmd.mock)
+            .await;
+        Mock::given(method("GET"))
+            .and(path("/rest/v1/sections"))
+            .respond_with(
+                ResponseTemplate::new(200)
+                    .set_body_raw(super::fixtures::SECTIONS, "application/json"),
+            )
+            .up_to_n_times(test.fetch_sections)
+            .mount(&cmd.mock)
+            .await;
 
-    cmd.cmd()?
-        .arg("list")
-        .arg("--nointeractive")
-        .env("RUST_BACKTRACE", "1")
-        .assert()
-        .success();
-    cmd.mock.verify().await;
+        let mut command = cmd.cmd()?;
+        for arg in test.args {
+            command.arg(arg);
+        }
+        command.env("RUST_BACKTRACE", "1").assert().success();
+        cmd.mock.verify().await;
+    }
 
     Ok(())
 }

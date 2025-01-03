3325bdfc2f81c5a415b6cec3d6589b0a85cb0365
Dominykas Djacenko <chaosteil@gmail.com>
2025-01-02 23:40:39 -0800

    Clean up error output for error messages.
    This removes some of the default boilerplate error messages in the
    color-eyre result.
    
    Additionally some cleanups in the various files affected.

--- src/bin/doist.rs
+++ src/bin/doist.rs
@@ -4,7 +4,10 @@ use doist::Arguments;
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    color_eyre::install()?;
+    color_eyre::config::HookBuilder::new()
+        .panic_section("consider reporting the bug at https://github.com/chaosteil/doist/issues")
+        .display_env_section(false)
+        .install()?;
     let args = Arguments::parse();
     args.exec().await
 }

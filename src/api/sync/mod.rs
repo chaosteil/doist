f0a43103bb2710bfb006d86dffcdaf82ef67b7ba
Dominykas Djačenko <chaosteil@gmail.com>
2025-01-19 13:57:14 -0800

    Delete deprecated sync package (#124)
    * Delete deprecated sync package
    
    * Bump release version

--- src/api/sync/mod.rs
+++
@@ -1,5 +0,0 @@
-//! This was an initial attempt on writing the sync API, but eventually I got too lazy to actually
-//! come through with it, given the REST API was enough for my needs. I'm leaving the code here for
-//! potential future use.
-mod data;
-mod state;
